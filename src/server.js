328954f2f19a9c85ade1a7c81b3a4519dc2f2e36
Josue Barrientos <jbarrientosv02@gmail.com>
2025-09-22 23:20:33 -0600

    Primeros Cambios

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const usersRoutes = require("./routes/users");
+const tasksRoutes = require("./routes/tasks");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", usersRoutes);
+app.use("/tasks", tasksRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
